fe9ba3bba37f73ddc4fcfc7044bd5c2b9925e27e
vitaliishevchuk <vitaliishevchuk123@gmail.com>
2023-10-03 11:58:31 +0300

    redirect add

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return redirect()->route('patterns');
+});
 Route::get('patterns/{pattern?}', \App\Http\Controllers\PatternsDescriptionController::class)->name('patterns');
 Route::get('definitions', \App\Http\Controllers\DefinitionController::class)->name('definitions');
